49150295f7451a2749e330426823ab3f1a739259
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-21 22:23:44 -0700

    Update events_today.py

--- events_today.py
+++ events_today.py
@@ -2,3 +2,13 @@ import datetime
 
 now = datetime.datetime.now()
 datetime.time(now.hour)
+now_hr = now.hour
+
+check = input("Press 'enter' to check your schedule.")
+
+if check == "":
+  #check now hr
+  #import events for this hr and rest of day
+  #print("")
+else:
+  print("ERROR!")
