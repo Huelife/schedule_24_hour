80a6e19a8551ba1c59e3da1ff936d615e42ef23f
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-22 22:00:57 -0700

    Update events_today.py
    added more code for functionality

--- events_today.py
+++ events_today.py
@@ -1,12 +1,20 @@
 import datetime
 
+import events.txt
+
 now = datetime.datetime.now()
 datetime.time(now.hour)
+
 now_hr = now.hour
+x = 24+now_hr
+p = now.hour
 
 check = input("Press 'enter' to check your schedule.")
 
 if check == "":
+  for i in range(x):
+    print(schedule_dict2[p],schedule_dict[p])
+    p += 1
   #check now hr
   #import events for this hr and rest of day
   #print("")
