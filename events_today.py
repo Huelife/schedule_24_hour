341546cbf1de4c8255bf1551782e5b3a25b45d05
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-22 23:22:31 -0700

    Update events_today.py
    combined events_today and events, made events empty

--- events_today.py
+++ events_today.py
@@ -1,12 +1,79 @@
-import datetime
+#events_today.py: current and future events
 
-import events.txt
+import datetime
 
 now = datetime.datetime.now()
 datetime.time(now.hour)
 
-now_hr = now.hour
-x = 24+now_hr
+hour_12_am = "Event"
+hour_01_am = "Event"
+hour_02_am = "Event"
+hour_03_am = "Event"
+hour_04_am = "Event"
+hour_05_am = "Event"
+hour_06_am = "Event"
+hour_07_am = "Event"
+hour_08_am = "Event"
+hour_09_am = "Event"
+hour_10_am = "Event"
+hour_11_am = "Event"
+
+hour_12_pm = "Event"
+hour_01_pm = "Event"
+hour_02_pm = "Event"
+hour_03_pm = "Event"
+hour_04_pm = "Event"
+hour_05_pm = "Event"
+hour_06_pm = "Event"
+hour_07_pm = "Event"
+hour_08_pm = "Event"
+hour_09_pm = "Event"
+hour_10_pm = "Event"
+hour_11_pm = "Event"
+
+#----------------------------------------------
+
+hour_12_am2 = "12am:"
+hour_01_am2 = "1am:"
+hour_02_am2 = "2am:"
+hour_03_am2 = "3am:"
+hour_04_am2 = "4am:"
+hour_05_am2 = "5am:"
+hour_06_am2 = "6am:"
+hour_07_am2 = "7am:"
+hour_08_am2 = "8am:"
+hour_09_am2 = "9am:"
+hour_10_am2 = "10am:"
+hour_11_am2 = "11am:"
+
+hour_12_pm2 = "12pm:"
+hour_01_pm2 = "1pm:"
+hour_02_pm2 = "2pm:"
+hour_03_pm2 = "3pm:"
+hour_04_pm2 = "4pm:"
+hour_05_pm2 = "5pm:"
+hour_06_pm2 = "6pm:"
+hour_07_pm2 = "7pm:"
+hour_08_pm2 = "8pm:"
+hour_09_pm2 = "9pm:"
+hour_10_pm2 = "10pm:"
+hour_11_pm2 = "11pm:"
+
+#--------------------------------------------
+
+schedule_event = (hour_12_am,hour_01_am,hour_02_am,hour_03_am,hour_04_am,
+                  hour_05_am,hour_06_am,hour_07_am,hour_08_am,hour_09_am,
+                  hour_10_am,hour_11_am,hour_12_pm,hour_01_pm,hour_02_pm,
+                  hour_03_pm,hour_04_pm,hour_05_pm,hour_06_pm,hour_07_pm,
+                  hour_08_pm,hour_09_pm,hour_10_pm,hour_11_pm)
+
+schedule_hr = (hour_12_am2,hour_01_am2,hour_02_am2,hour_03_am2,hour_04_am2,
+               hour_05_am2,hour_06_am2,hour_07_am2,hour_08_am2,hour_09_am2,
+               hour_10_am2,hour_11_am2,hour_12_pm2,hour_01_pm2,hour_02_pm2,
+               hour_03_pm2,hour_04_pm2,hour_05_pm2,hour_06_pm2,hour_07_pm2,
+               hour_08_pm2,hour_09_pm2,hour_10_pm2,hour_11_pm2)
+
+x = 24 - now.hour
 p = now.hour
 
 check = input("Press 'enter' to check your schedule.")
@@ -15,8 +82,5 @@ if check == "":
   for i in range(x):
     print(schedule_hr[p],schedule_event[p])
     p += 1
-  #check now hr
-  #import events for this hr and rest of day
-  #print("")
 else:
   print("ERROR!")
