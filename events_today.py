d11ec5707907aaa97865a76046d02d34baea2165
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-25 02:12:18 -0700

    Update events_today.py
    changed hour to hr for the 24 variables to be edited and the 24 variables in the tuple for simplification

--- events_today.py
+++ events_today.py
@@ -6,38 +6,38 @@ now = datetime.datetime.now()
 datetime.time(now.hour)
 
 #24 variables to edit for events
-hour_12_am = "12am: " + ""
-hour_01_am = "1am: " + ""
-hour_02_am = "2am: " + ""
-hour_03_am = "3am: " + ""
-hour_04_am = "4am: " + ""
-hour_05_am = "5am: " + ""
-hour_06_am = "6am: " + ""
-hour_07_am = "7am: " + ""
-hour_08_am = "8am: " + ""
-hour_09_am = "9am: " + ""
-hour_10_am = "10am: " + ""
-hour_11_am = "11am: " + ""
+hr_12_am = "12am: " + ""
+hr_01_am = "1am: " + ""
+hr_02_am = "2am: " + ""
+hr_03_am = "3am: " + ""
+hr_04_am = "4am: " + ""
+hr_05_am = "5am: " + ""
+hr_06_am = "6am: " + ""
+hr_07_am = "7am: " + ""
+hr_08_am = "8am: " + ""
+hr_09_am = "9am: " + ""
+hr_10_am = "10am: " + ""
+hr_11_am = "11am: " + ""
 
-hour_12_pm = "12pm: " + ""
-hour_01_pm = "1pm: " + ""
-hour_02_pm = "2pm: " + ""
-hour_03_pm = "3pm: " + ""
-hour_04_pm = "4pm: " + ""
-hour_05_pm = "5pm: " + ""
-hour_06_pm = "6pm: " + ""
-hour_07_pm = "7pm: " + ""
-hour_08_pm = "8pm: " + ""
-hour_09_pm = "9pm: " + ""
-hour_10_pm = "10pm: " + ""
-hour_11_pm = "11pm: " + ""
+hr_12_pm = "12pm: " + ""
+hr_01_pm = "1pm: " + ""
+hr_02_pm = "2pm: " + ""
+hr_03_pm = "3pm: " + ""
+hr_04_pm = "4pm: " + ""
+hr_05_pm = "5pm: " + ""
+hr_06_pm = "6pm: " + ""
+hr_07_pm = "7pm: " + ""
+hr_08_pm = "8pm: " + ""
+hr_09_pm = "9pm: " + ""
+hr_10_pm = "10pm: " + ""
+hr_11_pm = "11pm: " + ""
 
 #------------tuple for event
-schedule_event = (hour_12_am,hour_01_am,hour_02_am,hour_03_am,hour_04_am,
-                  hour_05_am,hour_06_am,hour_07_am,hour_08_am,hour_09_am,
-                  hour_10_am,hour_11_am,hour_12_pm,hour_01_pm,hour_02_pm,
-                  hour_03_pm,hour_04_pm,hour_05_pm,hour_06_pm,hour_07_pm,
-                  hour_08_pm,hour_09_pm,hour_10_pm,hour_11_pm)
+schedule_event = (hr_12_am,hr_01_am,hr_02_am,hr_03_am,hr_04_am,
+                  hr_05_am,hr_06_am,hr_07_am,hr_08_am,hr_09_am,
+                  hr_10_am,hr_11_am,hr_12_pm,hr_01_pm,hr_02_pm,
+                  hr_03_pm,hr_04_pm,hr_05_pm,hr_06_pm,hr_07_pm,
+                  hr_08_pm,hr_09_pm,hr_10_pm,hr_11_pm)
 
 #for loop variables
 hr_range = 24 - now.hour
